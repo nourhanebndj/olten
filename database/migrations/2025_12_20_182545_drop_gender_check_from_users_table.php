83a4b0db74837a3ba93ae768d8067869fa5428b8
smassaid <smassaid@techitaway.co>
2025-12-20 21:54:25 +0100

    ajouter utilisatuer + approuver + detail user

---
+++ database/migrations/2025_12_20_182545_drop_gender_check_from_users_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE users DROP CONSTRAINT IF EXISTS users_gender_check;');
+    }
+
+    public function down(): void
+    {
+        DB::statement("ALTER TABLE users ADD CONSTRAINT users_gender_check CHECK (gender IN ('f', 'm', 'o'));");
+    }
+};
