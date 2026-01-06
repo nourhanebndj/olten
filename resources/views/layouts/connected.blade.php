4a24a3d5945aeb93870e6b9ca763ee17a7259b51
smassaid <smassaid@techitaway.co>
2026-01-06 18:44:44 +0100

    correction deisgn

--- resources/views/layouts/connected.blade.php
+++ resources/views/layouts/connected.blade.php
@@ -9,6 +9,7 @@
       crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     <link rel="stylesheet" href="{{ asset('assets/css/style_connecter/style_connected.css') }}">
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('assets/css/style_connecter/style_connected.css') }}">
