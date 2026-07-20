ac6fba4f60b7c1c0de6d66c35e9ddd6a459642fb
tberrichi <tberrichi@techitaway.co>
2026-07-20 22:38:14 +0200

    changer l'ordre d'affichage des categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(Request $request) {
-        $categories = Category::latest()->get();
+        $categories = Category::orderByDesc('id')->get();
         $query = Ad::query()->where('is_approved', true);
         if ($request->filled('search')) {
             $query->where(function ($q) use ($request) {
